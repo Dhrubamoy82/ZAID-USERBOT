89f20094bcb4be0147afcd6964f4333c8b85612b
Dhrubamoy Mandal <122224525+Dhrubamoy82@users.noreply.github.com>
2023-01-25 21:47:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,6 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Welcome To Zaid Userbot'
 
-
+os.system("python3 -m Zaid")
 if __name__ == "__main__":
     app.run()
