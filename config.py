a49a0062a0ebbeb69031fa2bbaafa50c8f5c4d14
Dhrubamoy Mandal <122224525+Dhrubamoy82@users.noreply.github.com>
2023-01-29 10:45:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH", "") #optional
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5792746224").split()))
 OWNER_ID = int(getenv("OWNER_ID", "5792746224"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Zaid:Zaid@cluster0.589z3sh.mongodb.net/?retryWrites=true&w=majority")
-BOT_TOKEN = getenv("BOT_TOKEN", "5841845692:AAH0kjTW_o0OkC0b5sFz7I3Jy93REwUsD9Y")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "AQBiMZkArdBwWiyJt3t9j25R0PZaj4YFzMtGaVcA4I2i-BIpKVk85YuM7ZCtN1is7uCTtUxeIW_A5OWdakVDNoV_dqPYprOtB1YwDg9osFMuVdwlDwAOWLJ5exo38sc9VjXCvGRwiOtE4FklRGVPr5gAQyub-UC4nkAEI16IPkBpJoDKUdb13MXdhtkcSRG-Llb815ni_2Vv5IojRnf5azFQwloX5mRmHCSATbtXp9-hBu83xbVpBn-jQTq0RJOV7LAc_8g8eMiK-YpJJnTimQCa6t10useFHz89zl4ql-4cFWIX366qm4O3doPUf9qoNBoTRlsAFMxkBBaQiaXC9qMM0JnE6wAAAAFZRkrwAA")
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
